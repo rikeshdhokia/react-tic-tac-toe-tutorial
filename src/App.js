31ab86c53671304d0de7745b07eb3486fee9d638
Rikesh Dhokia <rikeshdhokia@gmail.com>
2017-10-02 12:45:31 +1100

    create a functional component out of sqaure

--- src/App.js
+++ src/App.js
@@ -70,14 +70,12 @@ class Board extends React.Component {
   }
 }
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props){
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 export default App;
